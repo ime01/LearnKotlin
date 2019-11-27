1e556bad1ecefa9279d70087c69edbe3b5a8c5e2
FLOWZ <ketuimevbore@yahoo.com>
2019-11-27 02:44:44 -0800

    Kotlin calculator app

--- app/src/main/java/com/flowz/learnkotlin/Main2Activity.kt
+++ app/src/main/java/com/flowz/learnkotlin/Main2Activity.kt
@@ -1,12 +1,19 @@
 package com.flowz.learnkotlin
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_main2.*
 
 class Main2Activity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main2)
+
+        button_guy.setOnClickListener {
+            val intent =  Intent(this, Main3Activity::class.java)
+            startActivity(intent)
+        }
     }
 }
